089b80b841e7c761278027d372fd1e7b08e10556
Administrator <khaitruong1301@gmail.com>
2021-03-01 17:31:49 +0700

    'dp'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="shadow-sm ..." >dsads</div>
+        <div className="shadow ..." >adsadas </div>
+        <div className="shadow-md ..." >dsad</div>
+        <div className="shadow-lg ..." > dsad</div>
+        <div className="shadow-xl ..." >dsada </div>
+        <div className="shadow-2xl ..." >dsada </div>
+      </div>
+
     </div>
   );
 }
