a1ceb520d796f6a2542236ba22368314ed394f45
macbook <khaitruong1301@gmail.com>
2021-03-10 17:08:27 +0700

    video3_tailwind_grid

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
+import DemoGrid from './TailWindComponent/DemoGrid';
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="shadow-sm ..." >dsads</div>
-        <div className="shadow ..." >adsadas </div>
-        <div className="shadow-md ..." >dsad</div>
-        <div className="shadow-lg ..." > dsad</div>
-        <div className="shadow-xl ..." >dsada </div>
-        <div className="shadow-2xl ..." >dsada </div>
-      </div>
+      <DemoGrid />
 
     </div>
   );
