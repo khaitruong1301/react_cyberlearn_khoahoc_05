7f652c68a5fc69db75ea61a35ade480aeef79f1a
macbook <khaitruong1301@gmail.com>
2021-04-14 16:22:02 +0700

    update_branch_movie

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import './App.css';
-import BaiTapTongHop from './BaiTapTongHop/BaiTapTongHop';
-import ChunkLodash from './Lodash/ChunkLodash';
-import CompareObjectArray from './Lodash/CompareObjectArray';
-import FillLodash from './Lodash/FillLodash';
-import FlattenLodash from './Lodash/FlattenLodash';
-import Include from './Lodash/Include';
-import JoinDemo from './Lodash/JoinDemo';
-import LastFirstLodash from './Lodash/LastFirstLodash';
-import SortLodash from './Lodash/SortLodash';
-import UniqLodash from './Lodash/UniqLodash';
-import BaiTapLayoutTailwindcss from './TailWindComponent/BaiTapLayoutTailwindcss';
-import BorderRadiusDemo from './TailWindComponent/BorderRadiusDemo';
-import CustomCss from './TailWindComponent/CustomCss';
-import DemoGrid from './TailWindComponent/DemoGrid';
-import FlexDemo from './TailWindComponent/FlexDemo';
-import PaddingMarginDemo from './TailWindComponent/PaddingMarginDemo';
-import ResponsiveDemo from './TailWindComponent/ResponsiveDemo';
-import TextBackGroundDemo from './TailWindComponent/TextBackGroundDemo';
-import WidthHeightDemo from './TailWindComponent/WidthHeightDemo';
 
 function App() {
   return (
     <div className="App">
-     
+
+
     </div>
   );
 }
